d72bc70497eb34a61c6aafdf70a34ef2de803e67
itsnotangel <angellaikaclarito04@gmail.com>
2025-05-29 07:07:52 +0800

    Import tkinter and TheQuizzler GUI class; initialize and run main quiz creator application window.

---
+++ the_quizzler_quiz_creator.py/quizzler_creator_main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from quizzler_creator_gui import TheQuizzler
+
+# Initializes the main Tkinter window and runs the event loop to start the GUI application
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TheQuizzler(root)
+    root.mainloop()
