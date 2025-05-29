8315560684a51269510036a29cc7d55ab8cd9f20
itsnotangel <angellaikaclarito04@gmail.com>
2025-05-29 15:29:08 +0800

    Create the main GUI class and initialize both the quiz logic and the root Tkinter window.

--- the_quizzler_quiz_app/quizzler_quiz_gui.py
+++ the_quizzler_quiz_app/quizzler_quiz_gui.py
@@ -1,3 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
 from quizzler_quiz_logic import QuizzlerQuizLogic
+
+class TheQuizzlerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("The Quizzler - The Ultimate Quiz Edition")
+        self.root.geometry("600x530")
+
+        self.logic = QuizzlerQuizLogic()
