e31233af745d218611419b33c458fbd42c7a0955
itsnotangel <angellaikaclarito04@gmail.com>
2025-05-29 10:22:09 +0800

    Define QuizzlerQuizLogic class and initialize quiz state variables.

--- the_quizzler_quiz_app/quizzler_quiz_logic.py
+++ the_quizzler_quiz_app/quizzler_quiz_logic.py
@@ -2,3 +2,11 @@ import random
 from tkinter import messagebox
 from quizzler_quiz_question import QuizQuestion
 from quizzler_quiz_utilities import load_quiz_questions_from_file
+
+# Handles quiz questions, current state, and scoring
+class QuizzlerQuizLogic:
+    def __init__(self):
+        self.quiz_questions = []
+        self.current_question = None
+        self.score_count = 0
+        self.total_questions = 0
