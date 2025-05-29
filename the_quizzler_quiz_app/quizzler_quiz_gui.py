2a251238344a27a52049ffbf0266f77e948aa70b
itsnotangel <angellaikaclarito04@gmail.com>
2025-05-29 15:30:57 +0800

    Add the main frame and header labels to the GUI layout.

--- the_quizzler_quiz_app/quizzler_quiz_gui.py
+++ the_quizzler_quiz_app/quizzler_quiz_gui.py
@@ -9,3 +9,12 @@ class TheQuizzlerApp:
         self.root.geometry("600x530")
 
         self.logic = QuizzlerQuizLogic()
+
+        main_frame = tk.Frame(root, bd=5, relief="groove", padx=20, pady=20)
+        main_frame.pack(fill="both", padx=20, pady=20)
+
+        main_title_label = tk.Label(main_frame, text="THE QUIZZLER", font=("Arial", 30, "bold"), fg="darkred")
+        main_title_label.pack(fill=tk.X, pady=(10, 0))
+
+        sub_title_label = tk.Label(main_frame, text="THE ULTIMATE QUIZ EDITION", font=("Arial", 20, "bold"), fg="darkred")
+        sub_title_label.pack(fill=tk.X, pady=(0, 10))
